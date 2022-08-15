c4d84703a39da97b33991a86bec4b495d29e22ac
ryandimba <ryan.dimba@student.moringaschool.com>
2022-08-15 20:25:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Ryan Dimba';
+const welcomeMessage = 'Welcome to Flatbook, Ryan Dimba!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
